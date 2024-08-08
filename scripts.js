6f7bf1be4a90d37937f02bc8e55f87a4c0b28c89
aph1010 <aph1010@gmail.com>
2024-08-08 12:25:30 -0500

    Añadido datos

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// Función para iniciar sesión
+function iniciarSesion() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    // Aquí puedes agregar la lógica para la autenticación de usuario
+    alert('Inicio de sesión no implementado.');
+}
+
+// Función para ver propiedades
+function verPropiedades() {
+    // Aquí puedes agregar la lógica para redirigir a la página de propiedades
+    alert('Función para ver todas las propiedades no implementada.');
+}
